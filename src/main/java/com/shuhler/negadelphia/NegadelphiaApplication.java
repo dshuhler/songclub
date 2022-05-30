7e1cf1ac53dbd29d47fb86925f60762987ebedd1
Dan <dshuhler@gmail.com>
2022-05-30 10:09:14 -0400

    Initial Commit

---
+++ src/main/java/com/shuhler/negadelphia/NegadelphiaApplication.java
@@ -0,0 +1,13 @@
+package com.shuhler.negadelphia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegadelphiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NegadelphiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shuhler/negadelphia/NegadelphiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuhler.negadelphia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegadelphiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
