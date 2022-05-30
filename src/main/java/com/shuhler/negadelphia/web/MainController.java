18333057895a305f0d4ed2aec5efe39feb92d987
Dan <dshuhler@gmail.com>
2022-05-30 16:13:29 -0400

    Add start/stop buttons

--- src/main/java/com/shuhler/negadelphia/web/MainController.java
+++ src/main/java/com/shuhler/negadelphia/web/MainController.java
@@ -1,10 +1,14 @@
 package com.shuhler.negadelphia.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -12,6 +16,9 @@ import java.util.List;
 
 @Controller
 public class MainController {
+
+
+    Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @Value("${welcome.message}")
     private String message;
@@ -32,6 +39,19 @@ public class MainController {
                     String name, Model model) {
 
         model.addAttribute("message", name);
+
+        return "index";
+    }
+
+    @PostMapping("/start")
+    public String start(Model model) {
+        logger.warn("Starting...");
+        return "index";
+    }
+
+    @PostMapping("/stop")
+    public String stop(Model model) {
+        logger.warn("Stopping...");
 
         return "index";
     }
