fde74694e9bd7e5d408c9c6afcc0c2273c5181e7
Dan <dshuhler@gmail.com>
2022-06-02 19:29:43 -0400

    Add twitter tester

---
+++ src/test/java/com/shuhler/negadelphia/TwitterTester.java
@@ -0,0 +1,25 @@
+package com.shuhler.negadelphia;
+
+import com.shuhler.negadelphia.domain.TwitterManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+/**
+ * Intended for expirimenting without bringing up the application, not actual testing
+ */
+@SpringBootTest
+@ActiveProfiles("local")
+public class TwitterTester {
+
+    @Autowired
+    private TwitterManager twitterManager;
+
+    @Test
+    void runTest() {
+        twitterManager.test();
+    }
+
+}
