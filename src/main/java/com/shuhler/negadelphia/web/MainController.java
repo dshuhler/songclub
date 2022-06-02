fb82f8d9c8cbd08836b69aeef301527963434e3e
Dan <dshuhler@gmail.com>
2022-06-01 23:39:24 -0400

    Clean up

--- src/main/java/com/shuhler/negadelphia/web/MainController.java
+++ src/main/java/com/shuhler/negadelphia/web/MainController.java
@@ -9,20 +9,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class MainController {
 
-    @Autowired
-    JobRunner jobRunner;
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
 
-    Logger logger = LoggerFactory.getLogger(MainController.class);
+    private JobRunner jobRunner;
+
+    @Autowired
+    public MainController(JobRunner jobRunner) {
+        this.jobRunner = jobRunner;
+    }
 
     @Value("${profile.name}")
     private String profileName;
